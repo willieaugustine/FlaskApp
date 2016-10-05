153d0fc38fb6d351c2e1873f0d4f0853d6b73dbb
Willie <mulwawillie@gmail.com>
2016-10-05 08:55:29 +0300

    updating jquery

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, json
+
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "Welcome!"
+    return render_template('index.html') #modifying the main method to render template
 if __name__ == "__main__":
     app.run()
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('singup.html')
+@app.route('/signUp',methods=['POST'])
+def signUp():
+    #read the posted values from the UI
+    _name =request.form['inputName']
+    _email = request.form['inputEmail']
+    _password =request.form['inputpassword']
+
+    #validate the received values
+    if _name and _email and _password:
+        return json.dumps({'html': '<span> All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span> Enter the Required fields</span>'})
+
